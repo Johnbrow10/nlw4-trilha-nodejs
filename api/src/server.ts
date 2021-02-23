ab97200a5a0291efc7e06724d8e155f055338a7c
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-02-22 23:16:21 -0300

    Criando Rotas e testando se retorna respostas.

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,4 +2,14 @@ import express from 'express';
 
 const app = express();
 
+
+app.get("/", (request, response) => {
+    return response.json({message: "hello mundo - nlw 04"});
+ })
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar 
+    return response.json({message: "Os dados foram salvos com o sucss"})
+})
+
 app.listen(3030, ()=> console.log("Server estar rodando"))
