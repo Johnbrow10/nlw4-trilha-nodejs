c0ca1e6dc267b4801ad1adb99018bed679c7f974
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-02-23 22:14:59 -0300

    Criando uma migration de users, rotas e voltando no console os dados do post

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database' 
+import { router } from './routes';
+
 
 const app = express();
 
-
-app.get("/", (request, response) => {
-    return response.json({message: "hello mundo - nlw 04"});
- })
-
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar 
-    return response.json({message: "Os dados foram salvos com o sucss"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3030, ()=> console.log("Server estar rodando"))
